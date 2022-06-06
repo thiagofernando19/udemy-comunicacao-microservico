e105bca35d98692505eb65f631aec5686d9374a2
thiagofernando19 <thiagofernando19@hotmail.com>
2022-06-06 16:32:07 -0300

    add jwt e jwt interceptor

---
+++ product-api/src/main/java/br/com/cursoudemy/productapi/config/interceptor/AuthInterceptor.java
@@ -0,0 +1,35 @@
+package br.com.cursoudemy.productapi.config.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import br.com.cursoudemy.productapi.modules.jwt.service.JwtService;
+import feign.Request.HttpMethod;
+
+public class AuthInterceptor implements HandlerInterceptor{
+	private static final String AUTHORIZATION = "Authorization";
+	
+	
+	@Autowired
+	private JwtService jwtService;
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, 
+			                 HttpServletResponse response,
+			                 Object handler) {
+		if(isOptions(request)) {
+			return true;
+		}
+		
+		var authorization = request.getHeader(AUTHORIZATION);
+		jwtService.validateAuthorization(authorization);
+		return true;
+	}
+	
+	private boolean isOptions(HttpServletRequest request) {
+		return HttpMethod.OPTIONS.name().equals(request.getMethod());
+	}
+}
---
+++ product-api/src/main/java/br/com/cursoudemy/productapi/modules/jwt/service/JwtService.java
@@ -0,0 +1,55 @@
+package br.com.cursoudemy.productapi.modules.jwt.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import br.com.cursoudemy.productapi.config.exception.AuthenticationException;
+import br.com.cursoudemy.productapi.modules.jwt.dto.JwtResponse;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+
+
+@Service
+public class JwtService {
+	
+	private static final String EMPTY_SPACE = " ";
+	private static final Integer TOKEN_INDEX = 1;
+	
+	@Value("${app-config.secrets.api-secret}")
+	private String apiSecret;
+
+	
+	public void validateAuthorization(String token) {
+		var accessToken = extractToken(token);
+		try {
+		
+			var claims = Jwts
+					.parserBuilder()
+					.setSigningKey(Keys.hmacShaKeyFor(apiSecret.getBytes()))
+					.build()
+					.parseClaimsJws(accessToken)
+					.getBody();
+
+			System.out.println("aquiiiiiiiiii");
+			var user = JwtResponse.getUser(claims);
+			if(user == null || user.getId() == null) {
+				throw new AuthenticationException("The user is not valid.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new AuthenticationException("Error while trying to proccess the Acces Token.");
+		}
+	}
+	
+	private String extractToken(String token) {
+		if(token == null) {
+			throw new AuthenticationException("The access token was not informed.");
+		}
+		if(token.contains(EMPTY_SPACE)) {
+			return token.split(EMPTY_SPACE)[TOKEN_INDEX];
+		}
+		
+		return token;
+	}
+}
