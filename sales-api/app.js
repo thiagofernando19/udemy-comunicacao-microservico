2101e330d2cea2e168b4080733cbf3f37e3334cf
thiagofernando19 <thiagofernando19@hotmail.com>
2022-06-07 22:01:59 -0300

    repair node modules

--- sales-api/app.js
+++ sales-api/app.js
@@ -1,11 +1,19 @@
 import express from "express";
 
+import { connect } from "./src/config/db/mongoDbConfig.js";
+import Order from "./src/modules/sales/model/Order";
+ 
+
 const app = express();
 
 const env = process.env;
 const PORT = env.PORT || 8082;
 
+connect();
+
 app.get("/api/status", (req, res) => {
+  let teste = await Order.find();
+     console.log(teste);
   return res.status(200).json({
     service: "Sales-api",
     status: "up",
